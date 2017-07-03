55515135a96f58404ee77c66f9e505a131f9594f
Muhammad Fahad Baig <muhammad.fb.79@gmail.com>
2017-07-03 18:35:55 +0500

    allow input event to handle smart key

--- core/java/android/view/KeyEvent.java
+++ core/java/android/view/KeyEvent.java
@@ -804,8 +804,10 @@ public class KeyEvent extends InputEvent implements Parcelable {
     public static final int KEYCODE_SYSTEM_NAVIGATION_LEFT = 282;
     /** Key code constant: Consumed by the system for navigation right */
     public static final int KEYCODE_SYSTEM_NAVIGATION_RIGHT = 283;
+    /** Key code constant: Huawei Smart Key (clickable back Fingerprint Scanner button) */
+    public static final int KEYCODE_SMART_KEY = 284;
 
-    private static final int LAST_KEYCODE = KEYCODE_SYSTEM_NAVIGATION_RIGHT;
+    private static final int LAST_KEYCODE = KEYCODE_SMART_KEY;
 
     // NOTE: If you add a new keycode here you must also add it to:
     //  isSystem()
